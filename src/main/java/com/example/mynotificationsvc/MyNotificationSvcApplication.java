1c8e57d8681e9cc5f710a1f5935cf6657fbd54cb
Michal Niedziela <michal.niedziela@ic-consult.com>
2021-01-26 16:41:06 +0000

    init project

---
+++ src/main/java/com/example/mynotificationsvc/MyNotificationSvcApplication.java
@@ -0,0 +1,13 @@
+package com.example.mynotificationsvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyNotificationSvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyNotificationSvcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mynotificationsvc/MyNotificationSvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mynotificationsvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyNotificationSvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
